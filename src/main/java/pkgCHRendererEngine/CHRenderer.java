34c74655b985047b22f83a11d330c65ab2282591
ColinH13 <117141355+ColinH13@users.noreply.github.com>
2025-03-15 18:48:10 -0700

    [CHRenderer.java] Started Renderer variables and methods

--- src/main/java/pkgCHRendererEngine/CHRenderer.java
+++ src/main/java/pkgCHRendererEngine/CHRenderer.java
@@ -1,4 +1,13 @@
 package pkgCHRendererEngine;
 
 public class CHRenderer {
+
+    private int shader_program;
+    private int NUM_COLS;
+    private Matrix4f viewProjMatrix;
+    private static int VPT;
+    privatge int[] winWidthHeight;
+    private private final
+
+    public CHRenderer(CHWindowManager) {}
 }
