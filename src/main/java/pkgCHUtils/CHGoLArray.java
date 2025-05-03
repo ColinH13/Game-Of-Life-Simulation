4852eb5ccb76bd1a4c3e7be8fe16aaf772411728
ColinH13 <117141355+ColinH13@users.noreply.github.com>
2025-05-02 19:52:06 -0700

    [CHGoLArray.java]
    - Added method signatures and parameters without implementation details

---
+++ src/main/java/pkgCHUtils/CHGoLArray.java
@@ -0,0 +1,15 @@
+package pkgCHUtils;
+
+public class CHGoLArray {
+
+    public int liveCellCount;
+
+    public CHGoLArray(String str) {}
+
+    public CHGoLArray(int var1, int var2) {}
+
+    public CHGoLArray(int var1, int var2, int var3) {}
+
+    public void onTickUpdate() {}
+
+}
