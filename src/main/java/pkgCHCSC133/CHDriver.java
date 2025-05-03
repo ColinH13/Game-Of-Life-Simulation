ab9515ec5ae9bcb217e969604d82c04583054767
ColinH13 <117141355+ColinH13@users.noreply.github.com>
2025-05-03 15:13:45 -0700

    [CHDriver.java, CHGoLArray.java]
    - Made minor changes

--- src/main/java/pkgCHCSC133/CHDriver.java
+++ src/main/java/pkgCHCSC133/CHDriver.java
@@ -1,9 +1,14 @@
 
 package pkgCHCSC133;
 import pkgCHRenderEngine.CHRenderer;
+import pkgCHUtils.CHGoLArray;
 import pkgCHUtils.CHWindowManager;
+
 public class CHDriver {
     public static void main(String[] args) {
+
+        // OLD Driver
+        /*
         final int numRows = 6, numCols = 7, polyLength = 50, polyOffset = 10,
                 polyPadding = 20;
         final int winWidth = (polyLength + polyPadding) * numCols + 2 * polyOffset;
@@ -14,5 +19,37 @@ public class CHDriver {
                 winOrgX, winOrgY);
         final CHRenderer myRenderer = new CHRenderer(myWM);
         myRenderer.render(polyOffset, polyPadding, polyLength, numRows, numCols);
+        */
+
+
+        // New Driver starts here
+
+
+        final int numRows = 6, numCols = 7, polyLength = 50, polyOffset = 10,
+                polyPadding = 20;
+        final int winWidth = (polyLength + polyPadding) * numCols + 2 * polyOffset;
+        final int winHeight = (polyLength + polyPadding) * numRows + 2 *
+                polyOffset;
+        final int winOrgX = 50, winOrgY = 80;
+
+        // Create WindowManager object
+        final CHWindowManager myWM = CHWindowManager.get(winWidth, winHeight,
+                winOrgX, winOrgY);
+
+        // Create GoLArray object
+        CHGoLArray goLArray = new CHGoLArray(numRows, numCols);
+
+
+        // Create Renderer object with rendering parameters and GoLArray object as parameter
+
+
+        // Send message to WindowManager to update the OpenGL Rendering Context to the Driver object
+
+
+        // Modify the driver to take the new constructor to GoLArray that takes a filename
+        // and creates a GoLArray from it
+
+
+
     } // public static void main(String[] args)
 } // public class Driver
--- src/main/java/pkgCHUtils/CHGoLArray.java
+++ src/main/java/pkgCHUtils/CHGoLArray.java
@@ -1,15 +1,31 @@
 package pkgCHUtils;
 
-public class CHGoLArray {
+public class CHGoLArray extends CHPingPongArrayLive{
 
     public int liveCellCount;
+    private int numLiveCells = 0; // default to 0
 
-    public CHGoLArray(final String myDataFile) {}
+    public CHGoLArray(final String myDataFile) {
+        super(16, 16, 0);
 
-    public CHGoLArray(final int rows, final int cols) {}
+        // read from file
+        readFromFile(myDataFile);
+    }
 
-    public CHGoLArray(int numRows, int numCols, int numAlive) {}
+    public CHGoLArray(final int rows, final int cols) {
+        super(rows, cols, 0);
+    }
+
+    public CHGoLArray(int numRows, int numCols, int numAlive) {
+
+
+    }
 
     public void onTickUpdate() {}
+
+    public void readFromFile(String fileName) {}
+
+
+    public int getLiveCellCount() {return liveCellCount;}
 
 }
