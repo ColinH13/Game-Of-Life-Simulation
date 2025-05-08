66e1757092f7d693e70252e943e565195fa12ae3
ColinH13 <117141355+ColinH13@users.noreply.github.com>
2025-05-08 12:33:59 -0700

    [CHPingPongArray.java, CHPingPongArrayLive.java]
    - Added PingPongArray classes

---
+++ src/main/java/pkgCHUtils/CHPingPongArray.java
@@ -0,0 +1,272 @@
+package pkgCHUtils;
+
+import java.util.Random;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.Arrays;
+
+public class CHPingPongArray {
+
+    int numRows;
+    int numCols;
+
+    int[][] liveArray;
+    int[][] nextArray;
+
+    int randMin;
+    int randMax;
+
+    Random rand = new Random();
+    private int DEFAULT_VALUE = 99;
+
+    protected record RCPair (int row, int col) {
+
+    } // private record RCPair(...)
+
+    RCPair pair;
+
+    public RCPair[] getNearestNeighborsArray(int orgRow, int orgCol) {
+        RCPair[] neighbors = new RCPair[8];
+
+        int nextRow = (orgRow + 1) % numRows;
+        int prevRow = (numRows + orgRow - 1) % numRows;
+        int nextCol = (orgCol + 1) % numCols;
+        int prevCol = (numCols + orgCol - 1) % numCols;
+
+        neighbors[0] = new RCPair(prevRow, prevCol); // Top-left
+        neighbors[1] = new RCPair(prevRow, orgCol); // Top-center
+        neighbors[2] = new RCPair(prevRow, nextCol); // Top-right
+        neighbors[3] = new RCPair(orgRow, prevCol); // Left
+        neighbors[4] = new RCPair(orgRow, nextCol); // Right
+        neighbors[5] = new RCPair(nextRow, prevCol); // Bottom-left
+        neighbors[6] = new RCPair(nextRow, orgCol); // Bottom-center
+        neighbors[7] = new RCPair(nextRow, nextCol); // Bottom-right
+
+        return neighbors;
+    } // getNearestNeighborsArray(...)
+
+
+    public CHPingPongArray(int numRows, int numCols, int randMin, int randMax) {
+        this.numRows = numRows;
+        this.numCols = numCols;
+
+        liveArray = new int[numRows][numCols];
+        nextArray = new int[numRows][numCols];
+
+        this.randMin = randMin;
+        this.randMax = randMax;
+
+        // Randomly initialize array
+        /*
+        for (int row = 0; row < numRows; ++row) {
+            for (int col = 0; col < numCols; ++col) {
+                nextArray[row][col] = rand.nextInt(randMin, randMax);
+            }
+        }*/
+        initArrays();
+    } // CHPingPongArray(...)
+
+    public void randomizeArray(int[] array) {
+        // Randomize elements in randArray
+        for (int i = 1; i < array.length; i++) {
+            // swap element at index 0 with random element at index in range (i, length - 1)
+            int index = rand.nextInt(i, array.length);
+            int tmp = array[0];
+            array[0] = array[index];
+            array[index] = tmp;
+        }
+
+        // populate nextArray with randomized elements
+        for (int i = 0; i < numRows * numCols; i++) {
+            int row = (i / numCols);
+            int col = i % numCols;
+            nextArray[row][col] = array[i];
+        }
+    } // randomizeArray(...)
+
+    // Does not replace the current values
+    public void randomize() {
+        int size = numRows * numCols;
+        int[] randArray = new int[size];
+
+        for (int row = 0; row < numRows; row++) {
+            for (int col = 0; col < numCols; col++) {
+                randArray[row * numCols + col] = liveArray[row][col];
+            }
+        }
+
+        randomizeArray(randArray);
+    } // randomize()
+    public void randomizeViaFisherYatesKnuth() {
+        randomize();
+    } // randomizeViaFisherYatesKnuth()
+
+    // Each number should be randomly distributed, each number appears exactly once.
+    public void randomizeInRange() {
+        int size = numRows * numCols;
+        int[] randArray = new int[size];
+
+        for (int i = 0; i < size; i++) {
+            randArray[i] = i;
+        }
+
+        randomizeArray(randArray);
+    } // randomizeInRange()
+
+    public void save(String dataFileName) {
+        try (PrintWriter writer = new PrintWriter(new FileWriter(dataFileName))) {
+
+            writer.println(DEFAULT_VALUE);
+            writer.println(numRows + " " + numCols);
+
+            for (int row = 0; row < numRows; row++) {
+                writer.print(row);
+
+                for (int col = 0; col < numCols; col++) {
+                    int value = liveArray[row][col];
+                    writer.print(" " + value);
+                }
+                writer.println();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    } // save(...)
+
+    // Save to a file names ppa_data.txt
+    public void save() {
+        save("ppa_data.txt");
+    } // save()
+
+    private void initArrays() {
+        for (int row = 0; row < numRows; row++) {
+            for (int col = 0; col < numCols; col++) {
+                nextArray[row][col] = DEFAULT_VALUE;
+            }
+        }
+
+
+    }
+
+    public boolean loadFile(String dataFilePath) {
+        boolean retVal = true;
+
+        try (BufferedReader myReader = new BufferedReader(new FileReader(dataFilePath))) {
+            String inputLine;
+            DEFAULT_VALUE = Integer.parseInt(myReader.readLine());
+            randMin = randMax = DEFAULT_VALUE;
+            inputLine = myReader.readLine();
+            int[] rowCol = Arrays.stream(inputLine.split("\\s+"))
+                    .mapToInt(Integer::parseInt)
+                    .toArray();
+            numRows = rowCol[0]; numCols = rowCol[1];
+
+            //TODO: implement initArrays()
+            initArrays();
+
+            int curRow = 0, rowLength = 0;
+            while ((inputLine = myReader.readLine()) != null) {
+                // Process each inputLine here
+                int[] readRow = Arrays.stream(inputLine.split("\\s+"))
+                        .mapToInt(Integer::parseInt)
+                        .toArray();
+                curRow = readRow[0];
+
+                for (int curCol = 1; curCol < readRow.length; ++curCol) {
+                    nextArray[curRow][curCol-1] = readRow[curCol];
+                }  //  for(int curCol = 0; curCol < readRow.length; ++curCol)
+            }  //  while ((inputLine = myReader.readLine()) != null)
+        } catch (IOException e) {
+            e.printStackTrace();
+            retVal = false;
+            return retVal;
+        }  // try ... catch.
+        retVal = true;
+        return retVal;
+    }  //  public boolean loadFile(...)
+
+
+    // TODO: May need to modify to return new array having iterated over liveArray
+    public int[][] getArray() {return liveArray.clone();}
+
+    public void copyToNextArray() {
+        for (int row = 0; row < numRows; row++) {
+            for (int col = 0; col < numCols; col++) {
+                nextArray[row][col] = liveArray[row][col]; // Copy value
+            }
+        }
+    } // copyToNextArray()
+
+    public void set(int row, int col, int newValue) {
+        nextArray[row][col] = newValue;
+    } // set(...)
+
+    public void setCell(int row, int col, int newValue) {
+        set(row, col, newValue);
+    } // setCell(...)
+
+    // Should update the nextArray to the sum of the nearest neighbor elements in the liveArray
+    public void updateToNNSum() {
+        // I think I basically do a for loop and iterate over all elements, and for each element set it to the sum of the nearest neighbor elements in the liveArray
+
+        int NNIndex = 0;
+        for (int row = 0; row < numRows; row++) {
+            for (int col = 0; col < numCols; col++) {
+                int nextRow = (row + 1) % numRows;
+                int prevRow = (numRows + row - 1) % numRows;
+                int nextCol = (col + 1) % numCols;
+                int prevCol = (numCols + col - 1) % numCols;
+
+                RCPair[] NN = getNearestNeighborsArray(row, col);
+
+                nextArray[row][col] = 0;
+                for (int i = 0; i < NN.length; i++) {
+                    nextArray[row][col] += liveArray[NN[i].row][NN[i].col];
+                }
+                NNIndex++;
+            }
+        }
+    } // updateToNNSum()
+
+    public void updateToNearestNNSum() {
+        updateToNNSum();
+    } // updateToNearestNNSum()
+
+    public void printArray() {
+        for (int row = 0; row < numRows; row++) {
+            System.out.print(row + " ");
+            for (int col = 0; col < numCols; col++) {
+                System.out.print(liveArray[row][col] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+    } // printArray()
+
+    public void swapLiveAndNext() {
+        int[][] temp = liveArray;
+        liveArray = nextArray;
+        nextArray = temp;
+    } // swapLiveAndNext()
+
+    public int getNumRows() {
+        return numRows;
+    }
+    public int getNumCols() {
+        return numCols;
+    }
+
+    public int getVal(int row, int col) {
+        return liveArray[row][col];
+    }
+
+    public int getDefaultValue() {
+        return DEFAULT_VALUE;
+    }
+
+}
+
---
+++ src/main/java/pkgCHUtils/CHPingPongArrayLive.java
@@ -0,0 +1,153 @@
+package pkgCHUtils;
+
+import java.util.Random;
+
+public class CHPingPongArrayLive extends CHPingPongArray {
+
+    public final int DEAD = 0;
+    public final int LIVE = 1;
+    int numLiveCells = 0;
+
+     public CHPingPongArrayLive(int numRows, int numCols, int numLiveCells) {
+        super(numRows, numCols, 0, 1);
+        this.numLiveCells = numLiveCells;
+
+        // Below code is to initialize the nextArray
+        // Initialize array with DEAD (0)
+        for (int i = 0; i < numLiveCells; i++) {
+            for (int j = 0; j < numCols; j++) {
+                nextArray[i][j] = DEAD;
+            }
+        }
+
+        // Create random array to assist in randomizing the nextArray
+        int arraySize = numRows * numCols;
+        int[] randomizeArray = new int[arraySize];
+
+        for (int i = 0; i < arraySize; i++) {
+            randomizeArray[i] = i;
+        }
+
+        // Randomize via FYK Algorithm
+         Random rand = new Random();
+        // TODO: test implementation for decrementing, then compare to incrementing values.
+        for (int i = 1; i < arraySize; i++) {
+            int j = rand.nextInt(i + 1);
+            // Swap elements at i and j
+            int temp = randomizeArray[i];
+            randomizeArray[i] = randomizeArray[j];
+            randomizeArray[j] = temp;
+        }
+
+        // Take the first n = numLiveCells elements, and fill in the corresponding indices of the nextArray
+         for (int i = 0; i < numLiveCells; i++) {
+             int index = randomizeArray[i];
+             int row = index / numCols;
+             int col = index % numCols;
+             nextArray[row][col] = LIVE;
+         }
+
+    }
+
+    // TODO: Created method, must now verify implementation
+    public int countLiveDegreeTwoNeighbors(int row, int col) {
+
+        int numLiveD2Numbers = 0;
+
+        // TODO: Decide between two implementations
+
+
+
+        // TODO: Implement code to check number of live degree 2 numbers
+        int prevRow = (row - 2 + numRows) % numRows;
+        int nextRow = (row + 2) % numRows;
+        int prevCol = (col - 2 + numCols) % numCols;
+        int nextCol = (col + 2) % numCols;
+
+        int[] neighbors = new int[16];
+        /*
+        neighbors[0] = liveArray[prevRow][prevCol];
+        neighbors[1] = liveArray[prevRow][prevCol + 1];
+        neighbors[2] = liveArray[prevRow][col];
+        neighbors[3] = liveArray[prevRow][col + 1];
+        neighbors[4] = liveArray;
+        neighbors[5] = ;
+        neighbors[6] = ;
+        neighbors[7] = ;
+        neighbors[8] = ;
+        neighbors[9] = ;
+        neighbors[10] = ;
+        neighbors[11] = ;
+        neighbors[12] = ;
+        neighbors[13] = ;
+        neighbors[14] = ;
+        neighbors[15] =;*/
+
+        neighbors[0]  = liveArray[(prevRow - 2 + numRows) % numRows][(prevCol - 2 + numCols) % numCols];
+        neighbors[1]  = liveArray[(prevRow - 2 + numRows) % numRows][(prevCol - 1 + numCols) % numCols];
+        neighbors[2]  = liveArray[(prevRow - 2 + numRows) % numRows][(prevCol + numCols) % numCols];
+        neighbors[3]  = liveArray[(prevRow - 2 + numRows) % numRows][(prevCol + 1 + numCols) % numCols];
+        neighbors[4]  = liveArray[(prevRow - 2 + numRows) % numRows][(prevCol + 2 + numCols) % numCols];
+
+        neighbors[5]  = liveArray[(prevRow - 1 + numRows) % numRows][(prevCol - 2 + numCols) % numCols];
+        neighbors[6]  = liveArray[(prevRow - 1 + numRows) % numRows][(prevCol + 2 + numCols) % numCols];
+
+        neighbors[7]  = liveArray[(prevRow + numRows) % numRows][(prevCol - 2 + numCols) % numCols];
+        neighbors[8]  = liveArray[(prevRow + numRows) % numRows][(prevCol + 2 + numCols) % numCols];
+
+        neighbors[9]  = liveArray[(prevRow + 1 + numRows) % numRows][(prevCol - 2 + numCols) % numCols];
+        neighbors[10] = liveArray[(prevRow + 1 + numRows) % numRows][(prevCol + 2 + numCols) % numCols];
+
+        neighbors[11] = liveArray[(prevRow + 2 + numRows) % numRows][(prevCol - 2 + numCols) % numCols];
+        neighbors[12] = liveArray[(prevRow + 2 + numRows) % numRows][(prevCol - 1 + numCols) % numCols];
+        neighbors[13] = liveArray[(prevRow + 2 + numRows) % numRows][(prevCol + numCols) % numCols];
+        neighbors[14] = liveArray[(prevRow + 2 + numRows) % numRows][(prevCol + 1 + numCols) % numCols];
+        neighbors[15] = liveArray[(prevRow + 2 + numRows) % numRows][(prevCol + 2 + numCols) % numCols];
+
+
+
+        // Iterate over neighbors array, increment numLiveD2Numbers if the element is 1, or alive.
+        for (int i = 0; i < neighbors.length; i++) {
+            if (neighbors[i] == LIVE) {
+                numLiveD2Numbers++;
+            }
+        }
+
+
+
+        // TODO: Remove test print statements
+        /*
+        for (int i = 0; i < numRows; i++) {
+            for (int j = 0; j < numCols; j++) {
+                System.out.print(liveArray[row][col]+ " ");
+            }
+            System.out.println();
+        }
+        System.out.println("for " + row + ", " + col + " = " + liveArray[row][col] + ":");
+
+        System.out.println("prevRow = " + prevRow);
+        System.out.println("nextRow = " + nextRow);
+        System.out.println("prevCol = " + prevCol);
+        System.out.println("nextCol = " + nextCol);*/
+
+        return numLiveD2Numbers;
+
+    } // public int countLiveDegreeTwoNeighbors(int row, int col)
+
+    public void printArray() {
+        for (int row = 0; row < numRows; row++) {
+            for (int col = 0; col < numCols; col++) {
+                System.out.print(liveArray[row][col]+ " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public int getValue(int row, int col) {
+         return super.getVal(row, col);
+    }
+
+
+
+
+}
